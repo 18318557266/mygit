b5a7624814dcaa168aa1e1c93bed92ba2d940419
jinqiancao <jinqiancao@etmcn.com>
2022-01-12 12:07:35 +0800

    master

---
+++ gateway/src/main/java/com/jinqiancao/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.jinqiancao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jinqiancao
+ * @date 2022/1/7 14:02
+ */
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+}
